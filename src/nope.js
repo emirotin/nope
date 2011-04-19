62d138a9eaacbbfc312d404d7eecc843376436a3
Eugene Mirotin <emirotin@gmail.com>
2011-04-19 23:13:07 +0300

    indents fixes

--- src/nope.js
+++ src/nope.js
@@ -3,16 +3,16 @@
  */
 
 var DEFAULT_SETTINGS = {
-	'port': 6868,
-	'get_password': null,
-	'put_password': null,
+    'port': 6868,
+    'get_password': null,
+    'put_password': null
 };
 
 var settings = require('./nope.conf.js').settings,
     express = require('express'),
-		io = require('socket.io'),
-		_ = require('underscore')._;
-		
+    io = require('socket.io'),
+    _ = require('underscore')._;
+        
 settings = _(DEFAULT_SETTINGS).extend(settings);
 
 var clients = {},
@@ -23,15 +23,15 @@ server.use(express.bodyParser());
 server.use(express.static(__dirname + '/public'));
 
 server.post('/push', function(req, res){
-	var password = req.body.password;
-	if (settings.put_password && password != settings.put_password) {
-		res.writeHead(403);
-		res.end('fail');
-		return;
-	}
-  res.writeHead(200);		
-	res.end('ok');
-	onPush(req.body.channel, req.body.client_id, req.body.type, req.body.body);
+    var password = req.body.password;
+    if (settings.put_password && password != settings.put_password) {
+        res.writeHead(403);
+        res.end('fail');
+        return;
+    }
+    res.writeHead(200);        
+    res.end('ok');
+    onPush(req.body.channel, req.body.client_id, req.body.type, req.body.body);
 });
 
 console.log('Listening on port ' + settings.port);
@@ -40,102 +40,103 @@ server.listen(settings.port);
 var socket = io.listen(server);
 
 socket.on('connection', function(client) {
-	clients[client.sessionId] = {
-		'socket_client': client,
-		'registered': false,		
-	};
-	
-	client.on('message', function(req) {
-		var type = req.type;
-		if (type == 'push') {
-			var password = req.body.password;
-      if (settings.put_password && password != settings.put_password) {
-				client.send({
-          'type': 'push',
-          'body': {
-            'result': 'fail',
-            'message': 'Wrong password'
-          }
-        });
-				return;
-	    }
-			onPush(req.body.channel, req.body.client_id, req.body.type, req.body.body);
-		}
-		else if (type == 'register') {
-		  onRegister(client, req.body);
-		}
-		else if (type == 'subscribe') {
-			onSubscribe(client, req.body);
-		}
-	});
+    clients[client.sessionId] = {
+        'socket_client': client,
+        'registered': false        
+    };
+    
+    client.on('message', function(req) {
+    	var type = req.type;
+        if (type == 'push') {
+            var password = req.body.password;
+            if (settings.put_password && password != settings.put_password) {
+                client.send({
+                	'type': 'push',
+                	'body': {
+                		'result': 'fail',
+                		'message': 'Wrong password'
+                	}
+                });
+                return;
+            }
+            onPush(req.body.channel, req.body.client_id, req.body.type, req.body.body);
+        }
+        else if (type == 'register') {
+        	onRegister(client, req.body);
+        }
+        else if (type == 'subscribe') {
+            onSubscribe(client, req.body);
+        }
+    });
 });
 
 function onPush(channel, client_id, type, message_body) {
-	if (!channel) {
-  	channel = '*';
-  }
-	if (channel in channels) 
-  	channel = channels[channel];
-  else {
-  	channels[channel] = [];
-		return;
-  }
-	var body = _({'client_id': client_id}).extend(message_body);
-	message = {
-		'type': type,
-		'client_id': client_id,
-		'body': body,
-	}
-	for (var i = 0, l = channel.length; i < l; i++) {
-		channel[i].send(message);
-	}	
+    if (!channel) {
+        channel = '*';
+    }
+    if (channel in channels) {
+        channel = channels[channel];
+    }
+    else {
+        channels[channel] = [];
+        return;
+    }
+    var body = _({'client_id': client_id}).extend(message_body);
+    var message = {
+        'type': type,
+        'client_id': client_id,
+        'body': body
+    }
+    for (var i = 0, l = channel.length; i < l; i++) {
+        channel[i].send(message);
+    }    
 }
 
 function onRegister(client, body) {
-  var client_id = body.id,
-      password = body.password;
-  if (settings.get_password && password != settings.get_password) {
+	var client_id = body.id,
+    	password = body.password;
+	if (settings.get_password && password != settings.get_password) {
 		client.send({
-		  'type': 'registration',
-			'body': {
-				'result': 'fail',
-				'message': 'Wrong password'
-			}
-	  })
-    return;
-  }
-	var client_record = clients[client.sessionId];
-	client_record.registered = true;
-	client_record.id = client_id;
-	channels['*'].push(client);
-  client.send({
-  	'type': 'registration',
-  	'body': {
-  		'result': 'ok'
-  	}
-  })
+			'type': 'registration',
+            'body': {
+                'result': 'fail',
+                'message': 'Wrong password'
+            }
+		});
+		return;
+	}
+    var client_record = clients[client.sessionId];
+    client_record.registered = true;
+    client_record.id = client_id;
+    channels['*'].push(client);
+    client.send({
+    	'type': 'registration',
+    	'body': {
+          'result': 'ok'
+      	}
+    })
 }
 
 function onSubscribe(client, body){
 	if (! clients[client.sessionId].registered) {
+        client.send({
+            'type': 'subscription',
+            'body': {
+                'result': 'fail',
+                'message': 'Not registered, register first.'
+            }
+        })
+        return;        
+    }
+    var channel = body.channel;
+    if (!(channel in channels)) {
+      channels[channel] = [];
+    }
+    channels[channel].push(client);
     client.send({
-			'type': 'subscription',
-			'body': {
-				'result': 'fail',
-				'message': 'Not registered, register first.'
-			}
-		})
-    return;		
-	}
-	var channel = body.channel;
-	if (!(channel in channels)) {
-  	channels[channel] = [];
-  }
-	channels[channel].push(client);
-  client.send({
-  	'type': 'subscription',
-  	'body': {
-  		'result': 'ok'
-  	}
-  })
+    	'type': 'subscription',
+    	'body': {
+          'result': 'ok'
+    	}
+    })
 }
